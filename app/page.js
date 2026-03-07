61bd1c39cf9faa02f629411f70463588f745b643
hyein89 <bihontai@gmail.com>
2026-03-08 05:42:29 +0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: 'center' }}>
+      <h1 style={{ fontSize: '2rem', fontWeight: '400', marginBottom: '1rem' }}>
+        Coming Soon
+      </h1>
+      <p style={{ color: '#666' }}>
+        Sistem sedang dalam tahap pengembangan.
+      </p>
+    </main>
+  );
+}
