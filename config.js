01ca6b61b811cf123ff5d627d24f513126e6163e
hyein89 <bihontai@gmail.com>
2026-03-08 06:53:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 export const siteConfig = {
-  domainName: 'tes.eu.org', // Ganti domain kamu di sini nantinya
-  siteName: 'Panel Redirect', // Nama website kamu
+  domainName: 'livesekhd.eu.org', // Ganti domain kamu di sini nantinya
+  siteName: 'Redirect...', // Nama website kamu
   
 };
