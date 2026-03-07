1b2cfbabaf2cb882c0753ebe3eab0e9e64bebce9
hyein89 <bihontai@gmail.com>
2026-03-08 06:18:48 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
+import { siteConfig } from '../config';
+
 export default function Home() {
   return (
     <main style={{ textAlign: 'center' }}>
       <h1 style={{ fontSize: '2rem', fontWeight: '400', marginBottom: '1rem' }}>
-        Coming Soon
+        {siteConfig.siteName}
       </h1>
       <p style={{ color: '#666' }}>
         Sistem sedang dalam tahap pengembangan.
