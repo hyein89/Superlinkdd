c66953ff407d1ff4f3ceca7bba337333505bab13
hyein89 <bihontai@gmail.com>
2026-03-08 14:05:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 export const siteConfig = {
   domainName: 'livesekhd.eu.org', // Ganti domain kamu di sini nantinya
-  siteName: 'Redirecting...', // Nama website kamu
+  siteName: 'Smartlink Manager', // Nama website kamu
   
 };
